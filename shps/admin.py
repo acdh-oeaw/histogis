e70d95b28d103e3f6a0c4806f3f38be9d4d5c84d
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-05-16 16:42:35 +0200

    registered shps classes in admin interface

--- shps/admin.py
+++ shps/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from leaflet.admin import LeafletGeoAdmin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Source, LeafletGeoAdmin)
+admin.site.register(TempSpatial, LeafletGeoAdmin)
+admin.site.register(TempStatialRel, LeafletGeoAdmin)
