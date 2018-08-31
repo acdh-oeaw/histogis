96f46e10be9295cca54ab7b9f3b8cba9be7b6a3d
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-08-31 11:46:04 +0200

    added team page and imprint implemented as link to öaw-imprint

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -35,5 +35,6 @@ PROJECT_METADATA = {
     'purpose_en': 'The purpose of HistoGIS is the publication of research data.',
     'version': '0.0.1',
     'matomo_id': '87',
-    'matomo_url': '//piwik.apollo.arz.oeaw.ac.at/'
+    'matomo_url': '//piwik.apollo.arz.oeaw.ac.at/',
+    'imprint': 'https://www.oeaw.ac.at/die-oeaw/impressum/',
 }
