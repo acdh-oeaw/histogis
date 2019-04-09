b17066f32768dd6e3c48c3ddb806b333d6a1a717
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-04-09 15:30:47 +0200

    added manamgent command to dump arche-md

---
+++ shps/management/commands/dump4arche.py
@@ -0,0 +1,13 @@
+from shps.to_arche import project_to_arche
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    help = """Creates ARCHE metadata RDF"""
+
+    def handle(self, *args, **options):
+        dump = project_to_arche().serialize('arche.xml', format='application/rdf+xml')
+
+        return dump
