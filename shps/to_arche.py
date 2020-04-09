3c40d6043f29e267c24d951b274f981bb6534d00
csae8092 <peter.andorfer@oeaw.ac.at>
2020-04-09 14:38:16 +0200

    rightsholder vecol

--- shps/to_arche.py
+++ shps/to_arche.py
@@ -172,7 +172,7 @@ def serialize_project():
     g.add((vecol, ARCHE.hasCreator, adueck))
     g.add((vecol, ARCHE.hasLicensor, URIRef('https://d-nb.info/gnd/1123037736')))
     g.add((vecol, ARCHE.hasOwner, URIRef('https://d-nb.info/gnd/1123037736')))
-
+    g.add((vecol, ARCHE.hasRightsHolder, URIRef('https://d-nb.info/gnd/1123037736')))
     g.add(
         (
             vecol,
